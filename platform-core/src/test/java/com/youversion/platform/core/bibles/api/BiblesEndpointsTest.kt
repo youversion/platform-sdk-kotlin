a137ea9b8e162d2b2ccb9105428bfdbc91f40cfb
dsrees <daniel.rees18@gmail.com>
2025-11-16 19:31:05 -0500

    fix: Fixed tests to use new host

--- platform-core/src/test/java/com/youversion/platform/core/bibles/api/BiblesEndpointsTest.kt
+++ platform-core/src/test/java/com/youversion/platform/core/bibles/api/BiblesEndpointsTest.kt
@@ -8,12 +8,12 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionsUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles?language_ranges=%2A",
+            "https://api.youversion.com/v1/bibles?language_ranges=%2A",
             BiblesEndpoints.versionsUrl().toString(),
         )
 
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles?language_ranges=eng",
+            "https://api.youversion.com/v1/bibles?language_ranges=eng",
             BiblesEndpoints.versionsUrl(languageRanges = setOf("eng")).toString(),
         )
     }
@@ -21,7 +21,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1",
+            "https://api.youversion.com/v1/bibles/1",
             BiblesEndpoints.versionUrl(versionId = 1).toString(),
         )
     }
@@ -29,7 +29,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionIndexUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/index",
+            "https://api.youversion.com/v1/bibles/1/index",
             BiblesEndpoints.versionIndexUrl(versionId = 1).toString(),
         )
     }
@@ -37,7 +37,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionBooksUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/books",
+            "https://api.youversion.com/v1/bibles/1/books",
             BiblesEndpoints.versionBooksUrl(versionId = 1).toString(),
         )
     }
@@ -45,7 +45,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionBookUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/books/GEN",
+            "https://api.youversion.com/v1/bibles/1/books/GEN",
             BiblesEndpoints.versionBookUrl(versionId = 1, book = "GEN").toString(),
         )
     }
@@ -53,7 +53,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionBookChaptersUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/books/GEN/chapters",
+            "https://api.youversion.com/v1/bibles/1/books/GEN/chapters",
             BiblesEndpoints.versionBookChaptersUrl(versionId = 1, book = "GEN").toString(),
         )
     }
@@ -61,7 +61,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionBookChapterUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/books/GEN/chapters/1",
+            "https://api.youversion.com/v1/bibles/1/books/GEN/chapters/1",
             BiblesEndpoints.versionBookChapterUrl(versionId = 1, book = "GEN", chapterId = "1").toString(),
         )
     }
@@ -69,7 +69,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionBookChapterVersesUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/books/GEN/chapters/1/verses",
+            "https://api.youversion.com/v1/bibles/1/books/GEN/chapters/1/verses",
             BiblesEndpoints.versionBookChapterVersesUrl(versionId = 1, book = "GEN", chapterId = "1").toString(),
         )
     }
@@ -77,7 +77,7 @@ class BiblesEndpointsTest {
     @Test
     fun `test versionBookChapterVerseUrl`() {
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/books/GEN/chapters/1/verses/1",
+            "https://api.youversion.com/v1/bibles/1/books/GEN/chapters/1/verses/1",
             BiblesEndpoints
                 .versionBookChapterVerseUrl(
                     versionId = 1,
@@ -92,12 +92,12 @@ class BiblesEndpointsTest {
     fun `test passageUrl`() {
         val reference = BibleReference(versionId = 1, bookUSFM = "GEN", chapter = 1)
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/passages/GEN.1?format=html&include_notes=true&include_headings=true",
+            "https://api.youversion.com/v1/bibles/1/passages/GEN.1?format=html&include_notes=true&include_headings=true",
             BiblesEndpoints.passageUrl(reference = reference).toString(),
         )
 
         assertEquals(
-            "https://api-staging.youversion.com/v1/bibles/1/passages/GEN.1?format=json&include_notes=true&include_headings=true",
+            "https://api.youversion.com/v1/bibles/1/passages/GEN.1?format=json&include_notes=true&include_headings=true",
             BiblesEndpoints.passageUrl(reference = reference, format = "json").toString(),
         )
     }
--- platform-core/src/test/java/com/youversion/platform/core/highlights/api/HighlightsEndpointsTests.kt
+++ platform-core/src/test/java/com/youversion/platform/core/highlights/api/HighlightsEndpointsTests.kt
@@ -7,18 +7,18 @@ class HighlightsEndpointsTests {
     @Test
     fun `test highlightsUrl`() {
         with(HighlightsEndpoints.highlightsUrl()) {
-            assertEquals("https://api-staging.youversion.com/v1/highlights", toString())
+            assertEquals("https://api.youversion.com/v1/highlights", toString())
         }
 
         with(HighlightsEndpoints.highlightsUrl(versionId = 1, passageId = "GEN.1")) {
-            assertEquals("https://api-staging.youversion.com/v1/highlights?version_id=1&passage_id=GEN.1", toString())
+            assertEquals("https://api.youversion.com/v1/highlights?version_id=1&passage_id=GEN.1", toString())
         }
     }
 
     @Test
     fun `test highlightsDeleteUrl`() {
         with(HighlightsEndpoints.highlightsDeleteUrl(versionId = 1, passageId = "GEN.1")) {
-            assertEquals("https://api-staging.youversion.com/v1/highlights/GEN.1?version_id=1", toString())
+            assertEquals("https://api.youversion.com/v1/highlights/GEN.1?version_id=1", toString())
         }
     }
 }
