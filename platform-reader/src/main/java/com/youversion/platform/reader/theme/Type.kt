d0ba66d7cbd0098989f4320a149c2a6b53e00b2a
Erik Pedersen <erikpedersen75@gmail.com>
2025-11-19 00:16:12 -0500

    feat: add Untitled Serif font family

---
+++ platform-reader/src/main/java/com/youversion/platform/reader/theme/Type.kt
@@ -0,0 +1,17 @@
+package com.youversion.platform.reader.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.youversion.platform.reader.R
+
+val UntitledSerif =
+    FontFamily(
+        Font(R.font.untitled_serif_app_regular, FontWeight.Normal),
+        Font(R.font.untitled_serif_app_regular, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.untitled_serif_app_medium, FontWeight.Medium),
+        Font(R.font.untitled_serif_app_medium_italic, FontWeight.Medium, FontStyle.Italic),
+        Font(R.font.untitled_serif_app_bold, FontWeight.Bold),
+        Font(R.font.untitled_serif_app_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    )
