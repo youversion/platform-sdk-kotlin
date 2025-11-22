da2ab178b396a73e59c79c54dd54aaee0346f78f
Erik Pedersen <erikpedersen75@gmail.com>
2025-11-22 17:38:14 -0500

    chore: remove unused imports

--- platform-core/src/main/java/com/youversion/platform/core/users/model/SignInWithYouVersionPermission.kt
+++ platform-core/src/main/java/com/youversion/platform/core/users/model/SignInWithYouVersionPermission.kt
@@ -1,8 +1,5 @@
 package com.youversion.platform.core.users.model
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
 enum class SignInWithYouVersionPermission(
     val rawValue: String,
 ) {
