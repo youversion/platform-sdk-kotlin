05e6727062f67444e6bd656bb8de2a00145d49d1
dsrees <daniel.rees18@gmail.com>
2025-11-23 20:02:09 -0500

    chore: removing unnused dependencies and bumped compose

--- examples/sample-android/build.gradle.kts
+++ examples/sample-android/build.gradle.kts
@@ -32,8 +32,6 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
         manifestPlaceholders["appAuthRedirectScheme"] = "youversionauth"
     }
 
@@ -73,11 +71,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
