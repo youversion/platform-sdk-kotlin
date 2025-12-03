4519e1e2169667cefe5fb31d2efe7dfdd5fb1850
Erik Pedersen <erikpedersen75@gmail.com>
2025-12-03 00:26:26 -0500

    chore: update documentation to include `idToken`

--- platform-core/src/main/java/com/youversion/platform/core/users/model/SignInWithYouVersionResult.kt
+++ platform-core/src/main/java/com/youversion/platform/core/users/model/SignInWithYouVersionResult.kt
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit
  * @property accessToken The OAuth access token for making authenticated API requests. Can be null if authentication failed at the final step.
  * @property expiryDate The calculated date and time when the access token will expire. Can be null if expiry information is not provided.
  * @property refreshToken The token used to refresh the access token once it expires.
+ * @property idToken A JSON Web Token (JWT) that contains the user's identity and profile information, proving that the user has been authenticated.
  * @property permissions A list of permissions granted by the user.
  * @property yvpUserId The unique YouVersion user ID.
  * @property name The user's name, if available and permission was granted.
