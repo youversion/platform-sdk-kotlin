2c5f023ecbfdf6b95948fc66ea6445ced0e382a3
Daniel Rees <daniel.rees18@gmail.com>
2025-12-15 09:17:39 -0500

    feat(theme): Added Aktiv Grotesk Font & Bible Theme Tokens (#26)
    * feat(tokens): Added typography tokens
    
    * feat(theme): Added properly named theme content from tokens
    
    * chore: Clean up BibleReaderTheme with existing conflict

---
+++ platform-reader/src/main/java/com/youversion/platform/reader/theme/tokens/BiblePaletteTokens.kt
@@ -0,0 +1,29 @@
+package com.youversion.platform.reader.theme.tokens
+
+import androidx.compose.ui.graphics.Color
+
+object BiblePaletteTokens {
+    val White = Color.White
+    val Gray5 = Color(0xFFF6F4F4)
+    val Gray10 = Color(0xFFEDEBEB)
+    val Gray15 = Color(0xFFDDDBDB)
+    val Gray20 = Color(0xFFBFBDBD)
+    val Gray30 = Color(0xFF636161)
+    val Gray35 = Color(0xFF474545)
+    val Gray40 = Color(0xFF353333)
+    val Gray45 = Color(0xFF232121)
+    val Gray50 = Color(0xFF121212)
+    val WarmNeutral = Color(0xFFF6EFEE)
+    val WarmNeutralDark = Color(0xFF373231)
+    val Green30 = Color(0xFF57A16C)
+    val Green30Dark = Color(0xFF4AAE67)
+    val Blue30 = Color(0xFF3D79C2)
+    val Blue30Dark = Color(0xFF2A85F4)
+    val Orange30 = Color(0xFFCA3E35)
+    val Orange30Dark = Color(0xFFF23B22)
+    val Yellow10 = Color(0xFFFEF5EB)
+    val Yellow30 = Color(0xFFF19C33)
+    val Yellow30Dark = Color(0xFFFFC24A)
+    val Red = Color(0xFFFF3D4D)
+    val RedDark = Color(0xFFF04C59)
+}
