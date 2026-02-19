ac7e459c2670e818a102bb7f658b09d530050e5e
Dennis D. Silva <dsilva609@gmail.com>
2026-02-19 09:20:23 -0500

    feat(bible-version): use default version for references (#52)
    Co-authored-by: Dennis Silva <dennis@MacBook-Pro.localdomain>

--- examples/sample-android/src/main/java/com/youversion/platform/ui/tabs/WidgetViewTab.kt
+++ examples/sample-android/src/main/java/com/youversion/platform/ui/tabs/WidgetViewTab.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.youversion.platform.core.BibleDefaults
 import com.youversion.platform.core.bibles.domain.BibleReference
 import com.youversion.platform.ui.components.SampleBottomBar
 import com.youversion.platform.ui.components.SampleDestination
@@ -23,7 +24,7 @@ import com.youversion.platform.ui.views.card.BibleCard
 fun WidgetViewTab(onDestinationClick: (SampleDestination) -> Unit) {
     val bibleReference =
         BibleReference(
-            versionId = 3034, // BSB
+            versionId = BibleDefaults.VERSION_ID,
             bookUSFM = "2CO",
             chapter = 1,
             verseStart = 3,
