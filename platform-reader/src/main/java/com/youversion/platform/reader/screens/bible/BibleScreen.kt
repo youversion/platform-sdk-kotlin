79b058434e6971c3f75d1894e39bede031b3aeb7
Dennis Silva <dennis@MacBook-Pro.localdomain>
2026-02-25 11:49:39 -0500

    update launch effect when banner type becomes null

--- platform-reader/src/main/java/com/youversion/platform/reader/screens/bible/BibleScreen.kt
+++ platform-reader/src/main/java/com/youversion/platform/reader/screens/bible/BibleScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -98,6 +99,12 @@ internal fun BibleScreen(
             BibleTextLoadingPhase.NOT_PERMITTED -> BibleReaderBannerType.VERSION_UNAVAILABLE
             else -> null
         }
+
+    LaunchedEffect(bannerType) {
+        if (bannerType == null) {
+            isBannerDismissed = false
+        }
+    }
 
     val topScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     val bottomScrollBehavior = BottomAppBarDefaults.exitAlwaysScrollBehavior()
