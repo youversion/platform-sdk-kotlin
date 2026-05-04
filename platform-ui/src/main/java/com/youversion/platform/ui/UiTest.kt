24e975fce42e7f32afdd17ffff1bf3e99e266ea2
Brenden Manquen <bmanquen1993@gmail.com>
2026-05-04 09:56:53 -0700

    chore(reader, ui): remove dead code

--- platform-ui/src/main/java/com/youversion/platform/ui/UiTest.kt
+++
@@ -1,9 +0,0 @@
-package com.youversion.platform.ui
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun UiTest(string: String) {
-    Text("Hello $string from UI Module")
-}
